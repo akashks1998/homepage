90ca0ef06b2414b4b56f551e8d1b7bed4c70fbd8
Akash kumar singh <akashks@iitk.ac.in>
2018-06-22 16:28:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,7 +45,6 @@ function mousePressed() {
     let bubble = new Bubble(mouseX, mouseY, random(-10, 10), random(-10, 10), 5);
     bubbles.push(bubble);
   }
-  alert(isMobileDevice());
 }
 
 function draw() {
@@ -121,8 +120,5 @@ function changeImg() {
   let im = select('#image-blurred-edge');
   im.style('background-image', "url('img/aka" + (Math.floor(cou / 100)) % 9 + ".jpg')")
 }
-function isMobileDevice() { 
-	return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
 
 
